7e3e4456f9b378ac07741eb1654a2e4e28281996
Alessandro Lima Alexandre <ale.limaalexandre@gmail.com>
2017-12-28 12:48:14 -0200

    alterando atibuto dataLancamento para Calendar

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,60 +1,61 @@
 package br.com.caelum.livraria.bean;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
-@SessionScoped
-public class LivroBean {
+@ViewScoped
+public class LivroBean implements Serializable {
 
-	private Livro livro = new Livro();
-	private Integer autorId;
+    private static final long serialVersionUID = 1L;
 
-	public Integer getAutorId() {
-		return autorId;
-	}
+    private Livro livro = new Livro();
+    private Integer autorId;
 
-	public void setAutorId(Integer autorId) {
-		this.autorId = autorId;
-	}
+    public void setAutorId(Integer autorId) {
+        this.autorId = autorId;
+    }
 
-	public Livro getLivro() {
-		return livro;
-	}
-	
-	public List<Autor> getAutoresDoLivro(){
-		return this.livro.getAutores();
-	}
+    public Integer getAutorId() {
+        return autorId;
+    }
 
-	public List<Livro> getLivros(){
-		return new DAO<Livro>(Livro.class).listaTodos();
-	}
-	
-	public void gravarAutor(){
-		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId); 
-		this.livro.adicionaAutor(autor);
-		
-		this.livro = new Livro(); //para limpar as informações do livro gravado
-	}
-	
-	public List<Autor> getAutores() {
-		return new DAO<Autor>(Autor.class).listaTodos();
-	}
+    public Livro getLivro() {
+        return livro;
+    }
 
-	public void gravar() {
-		System.out.println("Gravando livro " + this.livro.getTitulo());
+    public List<Livro> getLivros() {
+        return new DAO<Livro>(Livro.class).listaTodos();
+    }
 
-		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
-		}
+    public List<Autor> getAutores() {
+        return new DAO<Autor>(Autor.class).listaTodos();
+    }
 
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
-	}
+    public List<Autor> getAutoresDoLivro() {
+        return this.livro.getAutores();
+    }
 
+    public void gravarAutor() {
+        Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+        this.livro.adicionaAutor(autor);
+    }
+
+    public void gravar() {
+        System.out.println("Gravando livro " + this.livro.getTitulo());
+
+        if (livro.getAutores().isEmpty()) {
+            throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+        }
+
+        new DAO<Livro>(Livro.class).adiciona(this.livro);
+        this.livro = new Livro();
+    }
 }
